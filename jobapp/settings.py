48969f4e151b9b811c5f96055340ce411d7f7ef7
wynnson zheng <wzheng316@gatech.edu>
2025-09-17 17:31:50 -0400

    accounts & user scaffold

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles'
+    'home',
+    'accounts',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'jobapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'jobapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
