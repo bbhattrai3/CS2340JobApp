192b4aada2982acc7e910e9deea6899e8a631aef
bihan <bbhattrai3@gatech.edu>
2025-09-23 02:09:30 -0400

    added email features for recruit to seeker

--- recruiter/forms.py
+++ recruiter/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ContactCandidateForm(forms.Form):
-    subject = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'w-full'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'w-full', 'rows': 6}), help_text='aaaaaaaa')
+    subject = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'w-full ml-2'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'w-full', 'rows': 6, 'style': 'margin-left:0.5rem;'}))
